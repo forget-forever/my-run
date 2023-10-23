8b85bd0bdce6ec286e5d302c38997cff2a2a8f15
forget <hehezml@qq.com>
2023-10-23 23:20:54 +0800

    feat: add commane

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ require('tsconfig-paths/register')
 import chalk from "chalk";
 import { program } from "commander";
 import { resolve } from "path";
+import addShell from "./addShell";
 
 program.version(require("@root/package.json").version, '-v --version').usage('<command> <command> [name]');
 
@@ -12,7 +13,7 @@ program.command('run [name]').description('run loading....').action((name) => {
 });
 
 program.command('addStr [str] [name]').action((str, name) => {
-  
+  addShell(str, name)
 })
 
 // program.command('add [str]')
